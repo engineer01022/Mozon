4e27fb03e94f83d3f60d26147b575ccb9578a374
solimanoo <solimanooalfotahi.sa@gmail.com>
2025-08-20 16:37:54 +0300

    upload files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import Home from './components/Home';
+import AboutUs from './components/About-us';
+import Navbar from './components/navbar';
+import Work from './components/Work';
+import Service from './components/Service';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Solution from './components/Solution';
+import './App.css'
+class App extends Component{
+  render(){
+    return (
+      <div>
+<Navbar/>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Home />
+ <AboutUs />
+    <Work />
+    <Service />
+    <Solution />
+    <Contact />
+    <Footer />
+    </div>);
+  }
 }
-
 export default App;
